9d0a4bf4bd3419b065730dfeb2ea0966d2ceed1f
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2020-11-12 23:08:18 +0100

    No changes after this point.

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,11 @@ import * as http from 'http';
 import * as vscode from 'vscode';
 import { Repository } from './git';
 
-export async function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 	console.log('Successfully activated "WhatTheCommit"');
 
 	context.subscriptions.push(vscode.commands.registerCommand("wtc.getCommitMessage",
-		async (repository: Repository) => {
+		(repository: Repository) => {
 			http.get("http://www.whatthecommit.com/index.json", (resp) => {
 				let data = '';
 				resp.on('data', (chunk) => {
@@ -15,11 +15,12 @@ export async function activate(context: vscode.ExtensionContext) {
 				resp.on('end', () => {
 					let myData = JSON.parse(data);
 					repository.inputBox.value = myData.commit_message;
-					// vscode.window.showInformationMessage(myData.commit_message);
 				});
 			}).on("error", (err) => {
 				vscode.window.showErrorMessage("Unable to connect to whatthecommit.com: " + err.message, { modal: true });
 			});
 
 		}));
+		
+		vscode.commands.executeCommand('setContext', 'wtc.enabled', true);
 }
