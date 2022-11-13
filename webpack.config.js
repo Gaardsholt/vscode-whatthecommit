90b635eafc68e03653e8d010788d7044a4ebfa8c
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2022-11-13 21:54:00 +0100

    using https-browserify instead of stream-http
    Signed-off-by: Lasse Gaardsholt <lasse.gaardsholt@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const webExtensionConfig = {
 			// Webpack 5 no longer polyfills Node.js core modules automatically.
 			// see https://webpack.js.org/configuration/resolve/#resolvefallback
 			// for the list of Node.js core module polyfills.
-			"http": require.resolve("stream-http"),
+			"https": require.resolve("https-browserify"),
 			"url": require.resolve("url"),
 		}
 	},
@@ -65,4 +65,4 @@ const webExtensionConfig = {
 	},
 };
 
-module.exports = [ webExtensionConfig ];
+module.exports = [webExtensionConfig];
